e8cabed4327ea6792c0bac28a98dbb3c0877eb7b
samikshya <samidhakal245@gmail.com>
2022-09-05 10:47:46 +0545

    First Commit msg

---
+++ src/main/java/com/example/jwttry/JwttryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwttry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwttryApplication.class, args);
+	}
+
+}
